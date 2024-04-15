b19ab8a908a1e7bd121a12a8872e24a2460f3cc8
leey00nsu <dbstndla1212@naver.com>
2024-04-15 19:03:38 +0900

    refactor. fetcher 주석 추가

--- src/libs/fetcher.ts
+++ src/libs/fetcher.ts
@@ -9,12 +9,20 @@ interface FetcherProps {
   body?: BodyInit;
 }
 
+/**
+ * 해당 url로 method 방식의 요청을 보내고 응답을 Promise로 반환합니다.
+ * @param url   요청을 보낼 url
+ * @param method    요청 방식 (GET, POST, PUT, DELETE, PATCH)
+ * @param headers   요청 헤더
+ * @param body    요청 바디
+ * @returns
+ */
 export const fetcher = async <T>({
   url,
   method = 'GET',
   headers,
   body,
-}: FetcherProps) => {
+}: FetcherProps): Promise<ApiResponse<T>> => {
   const response = await fetch(url, {
     method,
     headers,
