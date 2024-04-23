b3dfe34e8bf11ed0c9d914c65c8591048ff31fc7
leey00nsu <dbstndla1212@naver.com>
2024-04-23 19:44:48 +0900

    refactor. 인물 업데이트 content 반영

--- src/services/supabase/character/update-character.ts
+++ src/services/supabase/character/update-character.ts
@@ -7,11 +7,17 @@ import {
 
 import { createHistory } from '../history/create-history';
 import { getUserData } from '../user/get-user-data';
+import { getCharacterById } from './get-character-by-id';
 
 export const updateCharacter = async (
   request: UpdateCharacterRequest,
 ): Promise<UpdateCharacterResponse> => {
   const { data: user } = await getUserData();
+
+  const { data: pastCharacter } = await getCharacterById({
+    projectId: request.projectId,
+    characterId: request.characterId,
+  });
 
   const updated = await prisma.characters.update({
     where: {
@@ -31,10 +37,24 @@ export const updateCharacter = async (
     throw new Error('인물 정보 업데이트에 실패했습니다. 다시 시도해주세요.');
   }
 
+  let content = '';
+
+  if (pastCharacter.name !== request.name) {
+    content += `이름: ${pastCharacter.name} -> ${request.name}\\n`;
+  }
+
+  if (pastCharacter.age !== request.age) {
+    content += `나이: ${pastCharacter.age} -> ${request.age}\\n`;
+  }
+
+  if (pastCharacter.description !== request.description) {
+    content += `설명: ${pastCharacter.description} -> ${request.description}\\n`;
+  }
+
   await createHistory({
     projectId: updated.projectId,
     title: `${request.name} 업데이트`,
-    content: '인물 정보가 업데이트 되었습니다.',
+    content,
   });
 
   return {
