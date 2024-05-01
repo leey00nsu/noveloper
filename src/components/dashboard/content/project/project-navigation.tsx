f4fc8b21f0010f49cc19dddf8838586160f7c563
leey00nsu <dbstndla1212@naver.com>
2024-05-01 21:25:57 +0900

    feat. FilterMenuButton 추가

--- src/components/dashboard/content/project/project-navigation.tsx
+++ src/components/dashboard/content/project/project-navigation.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 
 import SearchInput from '@/components/ui/input/search-input';
 
+import ProjectFilterButton from './project-filter-button';
 import ProjectListRefreshButton from './project-list-refresh-button';
 
 const ProjectNavigation = () => {
@@ -10,6 +11,7 @@ const ProjectNavigation = () => {
     <Group>
       <SearchInput placeholder="프로젝트를 검색해보세요." />
       <ProjectListRefreshButton />
+      <ProjectFilterButton />
       <Button component={Link} href="/dashboard/project/new">
         새로운 프로젝트 생성
       </Button>
---
+++ src/components/ui/button/filter-menu-button.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import { Button, Menu, Switch } from '@mantine/core';
+import { IoFilter } from 'react-icons/io5';
+import { TbCheck } from 'react-icons/tb';
+
+interface FilterMenuButtonProps {
+  filters: string[];
+  currentFilter: string;
+  currentOrder: 'asc' | 'desc';
+  onChageFilter: (filter: string) => void;
+  onToggleOrder: () => void;
+}
+
+const FilterMenuButton = ({
+  filters,
+  currentFilter,
+  currentOrder = 'asc',
+  onChageFilter,
+  onToggleOrder,
+}: FilterMenuButtonProps) => {
+  const clickFilterHanlder = (filter: string) => {
+    onChageFilter(filter);
+  };
+
+  return (
+    <Menu shadow="md" position="bottom-end">
+      <Menu.Target>
+        <Button leftSection={<IoFilter />}>필터</Button>
+      </Menu.Target>
+
+      <Menu.Dropdown>
+        <Menu.Label>
+          <Switch
+            checked={currentOrder === 'asc'}
+            onChange={onToggleOrder}
+            labelPosition="left"
+            label="오름차순"
+          />
+        </Menu.Label>
+
+        {filters.map((filter) => (
+          <Menu.Item
+            key={filter}
+            onClick={() => clickFilterHanlder(filter)}
+            rightSection={currentFilter === filter ? <TbCheck /> : null}
+          >
+            {filter}
+          </Menu.Item>
+        ))}
+      </Menu.Dropdown>
+    </Menu>
+  );
+};
+
+export default FilterMenuButton;
