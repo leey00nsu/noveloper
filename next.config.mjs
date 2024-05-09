2454adb8c10bfe2a3d94860584a3679195f90bff
leey00nsu <dbstndla1212@naver.com>
2024-05-09 20:40:54 +0900

    chore. output standalone 지정

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import createMDX from '@next/mdx';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  output: 'standalone',
 };
 
 const withMDX = createMDX({});
