4f2d8c120163bf4ad6f86b1e6bb87fa50fee541b
leey00nsu <dbstndla1212@naver.com>
2024-05-13 17:41:18 +0900

    style. FadeImage sizes 추가

--- src/components/ui/image/fade-image.tsx
+++ src/components/ui/image/fade-image.tsx
@@ -7,6 +7,7 @@ import tw from '@/libs/tw';
 const FadeImage = ({ className, ...props }: ImageProps) => {
   return (
     <Image
+      sizes="100vw"
       className={tw(className, 'opacity-0 transition-opacity duration-500')}
       onLoad={(image) => image.currentTarget.classList.remove('opacity-0')}
       {...props}
